efd55477ec0d52fbd88fbecceb93c42d2a9cc712
Mithun Raj <smithunraj007@gmail.com>
2023-11-06 19:52:37 +0530

    Todo App

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+import React, {useState} from "react";
 import './App.css';
 
-function App() {
+const App = () => {
+    const [inputValue, setInputValue] = useState('');
+    const [listItems, setListItems] = useState([]);
+
+    const handleInput = (e) => {
+        setInputValue(e.target.value);
+    }
+
+    const handleSubmit =(e) => {
+        e.preventDefault();
+        let tempList = listItems;
+        tempList.push(inputValue);
+        setListItems(tempList);
+        console.log(listItems);
+        setInputValue("");
+    }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="todo-app">
+        <div className="title">
+          <h1>Todo App</h1>
+        </div>
+        <div className="input-field">
+          <form onSubmit={handleSubmit} >
+            <input type="text" value={inputValue} placeholder="type here..." onChange={handleInput} ></input>
+            <button>Add</button>
+          </form>
+        </div>
+        <div className="list-items">
+            <ul>
+                {listItems.map((item) => (
+                    <li key={item} >{item}</li>
+                ))}
+            </ul>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
